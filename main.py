fd671e8201b5edc2475b23d81ade111de17c4be8
rtub5 <77343357+rtub5@users.noreply.github.com>
2021-04-07 12:50:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,9 @@ while operations_done != operations_to_resolve:
     
 x2 = datetime.datetime.now()
 
+
+print("Starting time" + x1)
+print("Seconds it took to perform the operations:")
 print(x2 - x1)
+print("") 
+print("Number of operations it made:" + operations_to_resolve)
